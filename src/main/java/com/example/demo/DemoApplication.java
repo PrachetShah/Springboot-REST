57f04c67de68b99beabfbda84157c18e8218620b
PrachetShah <prachetshah25@gmail.com>
2024-01-28 12:00:28 +0530

    Create Student Class and basic GET API

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(
+			new Student(
+				1L, 
+				"Prachet",
+				"prachetshah@gmail.com",
+				21,
+				LocalDate.of(2002, 12, 26)
+			)
+		);
+	}
+
+}
