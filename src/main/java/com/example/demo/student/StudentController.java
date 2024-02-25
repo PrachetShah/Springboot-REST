7b4f2aee49824406b62f4a397cdd3cb264f5af51
PrachetShah <prachetshah25@gmail.com>
2024-02-25 19:35:48 +0530

    Solving Dependency Injection by using Service Layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -2,15 +2,18 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This is controller class and RestController is needed to send responses to Service Layer
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,11 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+// import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+// This acts as a Service layer for our API Layer and Service helps us acheive that which is similar to component
+@Service
 public class StudentService {
 	public List<Student> getStudent(){
 		return List.of(
