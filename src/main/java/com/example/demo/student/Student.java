d1bf0d031595cf78b2465af39700be0d50e39d11
PrachetShah <prachetshah25@gmail.com>
2024-03-22 20:49:05 +0530

    Calculating age from dob using transient func

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 // This is the Student Class where we create student object for our application to use, create constructors,
 // getters and setters which are used by controller and service layer to serve outputs
@@ -29,25 +31,26 @@ public class Student {
     private Long id;
     private String name;
     private String email;
-    private Integer age;
     private LocalDate dob;
+
+    // No need to be a column in database, it should be calculated from DOB
+    @Transient
+    private Integer age;
 
     public Student(){
 
     }
 
-    public Student(Long id, String name, String email, Integer age, LocalDate dob){
+    public Student(Long id, String name, String email, LocalDate dob){
         this.id = id;
         this.name = name;
         this.email = email;
-        this.age = age;
         this.dob = dob;
     }
 
-    public Student(String name, String email, Integer age, LocalDate dob){
+    public Student(String name, String email, LocalDate dob){
         this.name = name;
         this.email = email;
-        this.age = age;
         this.dob = dob;
     }
 
@@ -76,7 +79,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
