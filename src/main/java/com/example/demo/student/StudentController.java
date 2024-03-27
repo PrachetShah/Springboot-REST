6eb4a77dc8d9969e531fd44771af950c0fea2a89
PrachetShah <prachetshah25@gmail.com>
2024-03-27 21:07:38 +0530

    Completed with Delete and Put requests, using Transactional Annotation

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,10 +3,14 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // This is controller class and RestController is needed to send responses to Service Layer and create paths
@@ -36,5 +40,18 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path="{studentId}")
+    public String deleteStudent(@PathVariable("studentId") Long studentId){
+        String result = studentService.deleteStudent(studentId);
+        return result;
+    }
+
+    @PutMapping(path="{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, 
+                            @RequestParam(required = false) String name, 
+                            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name , email);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,11 +2,13 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /*
 This acts as a Service layer for our API Layer and Service helps us acheive that which is similar to component
@@ -37,6 +39,38 @@ public class StudentService {
 			throw new IllegalStateException("Email Taken");
 		}
 		studentRepository.save(student);
+	}
+
+	public String deleteStudent(Long studentId){
+		boolean exists = studentRepository.existsById(studentId);
+		if(!exists){
+			throw new IllegalStateException("No Student with Id: "+studentId+ " does not Exists");
+		}
+		studentRepository.deleteById(studentId);
+		return "Student with Id: " + studentId + " deleted";
+	}
+
+	// because of this @Transactional annotation, meaning that any database operations performed within the marked method or 
+	// class will be executed within a transaction. If the transaction is successful, the changes will be committed to the database.
+	// we can directly use getters and setters to make changes, as it goes into manage state and there is no need to use repository/DB cmds
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email){
+		Student student = studentRepository.findById(studentId).orElseThrow(()-> 
+			new IllegalStateException("No Student with Id: "+studentId+ " does not Exists")
+		);
+		
+		if(name != null && name.length()>0 && !Objects.equals(student.getName(), name)){
+			student.setName(name);
+		}
+
+		if(email != null && email.length()>0 && !Objects.equals(student.getEmail(), email)){
+			// check here for passed email to check whether email exists in DB or not
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if(studentOptional.isPresent()){
+				throw new IllegalStateException("Email Taken");
+			}
+			student.setEmail(email);
+		}
 	}
 
 }
