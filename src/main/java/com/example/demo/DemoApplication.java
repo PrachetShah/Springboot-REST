bc8752af6635ef860a2c758c2e6794d5e7343e4e
PrachetShah <prachetshah25@gmail.com>
2024-04-04 10:39:22 +0530

    trying to deploy package

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
